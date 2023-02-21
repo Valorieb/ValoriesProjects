50d69db174a54b6db81d8871e622e3c0d8030851
Valorieb <62398793+Valorieb@users.noreply.github.com>
2023-02-21 13:37:02 -0500

    Add files via upload
    initial commit

---
+++ spring-boot-candy-ecommerce/src/main/java/com/candystore/ecommerce/SpringBootCandyEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.candystore.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCandyEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCandyEcommerceApplication.class, args);
+	}
+
+}
